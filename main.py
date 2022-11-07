94db059333e61508fb6551b6dc2194f5eee31e1c
Yun Bom Yi <ahybi986@naver.com>
2022-11-07 18:36:42 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from requests import get
+
+websites = (
+    "google.com",
+    "aribnb.com",
+    "https://twitther.com",
+    "facebook.com",
+    "https://tiktok.com"
+)
+
+results = {
+}
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    response = get(website)
+    if response.status_code == 200 :
+        results[website] = "OK"
+    else :
+        results[website] = "FAILED"
+
+print(results)
