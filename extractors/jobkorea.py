72f9c6d9fb59b5082ff14e380fa8c253690eaa30
Yun Bom Yi <ahybi986@naver.com>
2022-11-25 10:24:12 +0900

    잡코리아
    post info 스크랩 (main.py는 차후에)

--- extractors/jobkorea.py
+++ extractors/jobkorea.py
@@ -10,8 +10,15 @@ def extract_jobkorea_jobs(keyword):
     if response.status_code != 200:
         print(f'Error code : {response.status_code}')
     else:
-        soup: BeautifulSoup(response.text, 'html.parser')
-        
+        soup = BeautifulSoup(response.text, "html.parser")
+        jobs = soup.find_all('li', class_="list-post")
+        for job_post in jobs:
+            job_info = job_post.find_all('div', class_="post-list-info")
+            print(job_info)
+            print(".")
+            print(".")
+            print(".")
+
 
 
 extract_jobkorea_jobs("python")
--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from extractors.jobkorea import extract_jobkorea_jobs
 
+"""                 job_data = {
+                    "title" : title.replace(",", " "),
+                    "company" : company.string.replace(",", " "),
+                    "location" : location.string.replace(",", " "),
+                    "link" : f"https://kr.indeed.com{link}",
+                } """
+
 keyword = input("what do you want to search for?")
-
-file = open(f'{keyword}.csv'. "w")
-
-
 
 indeed = extract_indeed_jobs(keyword)
 wwr = extract_wwr_jobs(keyword)
 # jobkorea = extract_jobkorea_jobs(keyword)
 
 jobs = indeed + wwr
+
+file = open(f'{keyword}.csv', "w")
+file.write("Title, Company, Location, Link/n")
+
+
 
 
