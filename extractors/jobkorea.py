608332521c162c09c20d3ea97f2b2a3ce055c7e9
Yun Bom Yi <ahybi986@naver.com>
2022-11-26 10:45:27 +0900

    title 추출완료
    company 추출 작업중

--- extractors/jobkorea.py
+++ extractors/jobkorea.py
@@ -3,8 +3,10 @@
 from requests import get
 from bs4 import BeautifulSoup
 
+# Title, Company, Location, Link
 
 def extract_jobkorea_jobs(keyword):
+    results = []
     base_url = 'https://www.jobkorea.co.kr/Search/?stext='
     response = get(f'{base_url}{keyword}')
     if response.status_code != 200:
@@ -14,12 +16,15 @@ def extract_jobkorea_jobs(keyword):
         jobs = soup.find_all('li', class_="list-post")
         for job_post in jobs:
             job_info = job_post.find_all('div', class_="post-list-info")
-            print(job_info)
+            # 회사이름 추출 (어떻게??)
+            job_corp = job_post.find_all('div', class_="post-list-corp")
+            print(job_corp)
             print(".")
             print(".")
-            print(".")
-
-
+            for info in job_info:
+                a = info.find_all('a')
+                # title 결과 추출
+                title = a[0].string
 
 extract_jobkorea_jobs("python")
 
